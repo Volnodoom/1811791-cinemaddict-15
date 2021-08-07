fa9b814ad8ce388d3c5d5dedb43817d4d5789d49
Alexander <Bdiyoz@mail.ru>
2021-08-07 05:23:34 +0400

    module2 task1 have complited except of performance for showMore button

--- src/main.js
+++ src/main.js
@@ -12,7 +12,6 @@ const MOST_COMMENTED_COUNT =2;
 
 const films = new Array (FILMS_CARDS_COUNT).fill(' ').map(generateFilmInfo);
 const filter = generateFilter(films);
-console.log (filter[0].count.length);
 
 const bodyPart = document.body;
 const footerPart = bodyPart.querySelector('.footer__statistics');
@@ -23,53 +22,46 @@ const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
-const loadingState = () => {
-  render(mainOfBody, createLoadingTemplate(), 'beforeend');
-  render(footerPart, footerTemplateForBlankWithoutMovie, 'beforeend');
-};
-
-const emptyState = () => {
-  render(mainOfBody, createEmptyBlank(), 'beforeend');
+const nonOperationalStateLoading = () => {
+  render(mainOfBody, createEmptyTemplate(filter, 'loading'), 'beforeend');
   render(footerPart, footerTemplateForBlankWithoutMovie, 'beforeend');
 };
 
 const operationalState = () => {
-  render(mainOfBody, createMovieCardFramework(filter), 'beforeend');
-//   render(footerPart, footerTemplateForUpToDate, 'beforeend');
-//   render(headerOfBody, headerAvatar, 'beforeend');
+  render(mainOfBody, createOperationalFramework(filter), 'beforeend');
+  render(footerPart, footerTemplateForUpToDate, 'beforeend');
+  render(headerOfBody, createHeaderAvatar(), 'beforeend');
 
-//   const containerDivInMovies = mainOfBody.querySelector('.films-list__container');
+  const containerDivInMovies = mainOfBody.querySelector('.films-list__container');
 
-//   for (let ind = 0; ind <FILMS_CARDS_COUNT; ind++) {
-//     render(containerDivInMovies, creatCardTemplate (films[ind]), 'beforeend');
-//   }
-//   render(containerDivInMovies, createButtonShowMore(), 'afterend');
+  for (let ind = 0; ind <FILMS_CARDS_COUNT; ind++) {render(containerDivInMovies, creatCardTemplate (films[ind]), 'beforeend');}
+  render(containerDivInMovies, createButtonShowMore(), 'afterend');
 
-//   const sectionMovies = mainOfBody.querySelector('.films-list');
+  const sectionMovies = mainOfBody.querySelector('.films-list');
 
-//   render(sectionMovies, createMovieCardFrameworkMost(), 'afterend');
-//   render(sectionMovies, createMovieCardFrameworkTop(), 'afterend');
+  render(sectionMovies, createFrameworkForExtraMostComments(), 'afterend');
+  render(sectionMovies, createFrameworkForExtraTop(), 'afterend');
 
-//   const containerSectionExtraMovies = mainOfBody.querySelectorAll('.films-list__container');
-//   // ----> for TOP and MostCommented movies
-//   for (let ind = 0; ind <TOP_FILMS_COUNT; ind++) {
-//     render(containerSectionExtraMovies[1], creatCardTemplate (films.slice().sort((aInd,bInd) => {return bInd.totalRating - aInd.totalRating;})[ind]), 'beforeend');
-//   }
-//   for (let ind = 0; ind <MOST_COMMENTED_COUNT; ind++) {
-//     render(containerSectionExtraMovies[2], creatCardTemplate (films.slice().sort((aInd,bInd) => {return bInd.comments.length - aInd.comments.length;})[ind]), 'beforeend');
-//   }
+  const containerSectionExtraMovies = mainOfBody.querySelectorAll('.films-list__container');
+  // ----> for TOP and MostCommented movies
+  for (let ind = 0; ind <TOP_FILMS_COUNT; ind++) {
+    render(containerSectionExtraMovies[1], creatCardTemplate (films.slice().sort((aInd,bInd) => {return bInd.totalRating - aInd.totalRating;})[ind]), 'beforeend');
+  }
+  for (let ind = 0; ind <MOST_COMMENTED_COUNT; ind++) {
+    render(containerSectionExtraMovies[2], creatCardTemplate (films.slice().sort((aInd,bInd) => {return bInd.comments.length - aInd.comments.length;})[ind]), 'beforeend');
+  }
 };
 
-// const popup = () => {
-//   render(bodyPart, createPopupTemplate(films[0]), 'beforeend');
+const popup = () => {
+  render(bodyPart, createPopupTemplate(films[0]), 'beforeend');
 
-//   const containerForUserCom = bodyPart.querySelector('.film-details__comments-list');
-//   render(containerForUserCom, getUsersComments(films[0]), 'beforeend');
-// };
+  const containerForUserCom = bodyPart.querySelector('.film-details__comments-list');
+  render(containerForUserCom, getUsersComments(films[0]), 'beforeend');
+};
 
 
 operationalState();
-// loadingState();
+// nonOperationalStateLoading();
 //popup();
 // emptyState();
 
--- src/model/movie-card.js
+++ src/model/movie-card.js
@@ -28,6 +28,7 @@ const calculateTime = (movieDuration) => {
 const creatCardTemplate = (film) => {
   const {title, totalRating, runtime, genre, poster, description} = film;
   const {data} = film.release;
+  const {watchlist, alreadyWatched, favorite} = film.userDetails;
 
   const genreLine = genre.join(', ');
   const altPoster = title;
@@ -37,6 +38,14 @@ const creatCardTemplate = (film) => {
     descriptionLine += description.slice().substring(0, 139);
     descriptionLine += '...';
   } else {descriptionLine = description;}
+
+  let watchlistClassName = '';
+  let historyClassName = '';
+  let favoriteClassName = '';
+
+  if (watchlist) {watchlistClassName = 'film-card__controls-item--active';} else {watchlistClassName = '';}
+  if (alreadyWatched) {historyClassName = 'film-card__controls-item--active';} else {historyClassName = '';}
+  if (favorite) {favoriteClassName = 'film-card__controls-item--active';} else {favoriteClassName = '';}
 
   return `<article class="film-card">
   <h3 class="film-card__title">${title}</h3>
@@ -50,9 +59,9 @@ const creatCardTemplate = (film) => {
   <p class="film-card__description">${descriptionLine}</p>
   <a class="film-card__comments">${film.comments.length} comments</a>
   <div class="film-card__controls">
-    <button class="film-card__controls-item film-card__controls-item--add-to-watchlist" type="button">Add to watchlist</button>
-    <button class="film-card__controls-item film-card__controls-item--mark-as-watched" type="button">Mark as watched</button>
-    <button class="film-card__controls-item film-card__controls-item--favorite" type="button">Mark as favorite</button>
+    <button class="film-card__controls-item film-card__controls-item--add-to-watchlist ${watchlistClassName}" type="button">Add to watchlist</button>
+    <button class="film-card__controls-item film-card__controls-item--mark-as-watched ${historyClassName}" type="button">Mark as watched</button>
+    <button class="film-card__controls-item film-card__controls-item--favorite ${favoriteClassName}" type="button">Mark as favorite</button>
   </div>
 </article>`;
 };
